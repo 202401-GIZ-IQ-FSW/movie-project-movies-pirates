399b37026507561dc9d7d2e1c1b6f395aa3569cd
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-03-24 17:38:42 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
-import Image from "next/image"
-
+import ListActors from "@/components/ListActors";
+import ListMovies from "@/components/ListMovies";
+import Image from "next/image";
+ 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  
-    </main>
+   
+
+
+    <>
+    
+ <ListMovies/>
+    </>
   );
 }
